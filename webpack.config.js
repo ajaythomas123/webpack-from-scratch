17db41c2938c03fabfb33f1a2d2ca4208613ad05
Ajay Thomas <ajaythomas123@gmail.com>
2024-05-09 22:51:26 +0200

    10. Set up live-reloading with webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'none',
   context: path.resolve(__dirname, '.'),
-  // The entry paths are all relative to the context path
+  // The entry paths are all relative to the context path.
   entry: {
     mainApp: './src/client/mainApp/index.js',
     popupApp: './src/client/popupApp/index.js',
@@ -19,9 +19,29 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack from Scratch',
-      // The filename path is relative to output.path
+      // The filename path is relative to output.path.
       filename: 'index.html'
     })
   ],
-  watch: true
+  devServer: {
+    static: {
+      /**
+       * webpack-dev-server stores the output files in memory.
+       * When `npx webpack serve` is run, this directory won't be generated in the filesystem if it doesn't already exist.
+       */
+      directory: path.join(__dirname, 'dist'),
+      // Watches for changes in the static directory
+      watch: false
+    },
+    // For live reloading to work, hot module replacement has to be disabled.
+    hot: false,
+    /**
+     * liveReload refreshes the browser everytime file changes are detected. This happens when
+     * - Webpack regenerates the bundled files.
+     * - When a there is a file change in a static directory that has the the watch property set to true. 
+     */
+    liveReload: true,
+    compress: true,
+    port: 9000,
+  },
 }
